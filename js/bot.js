3d2a3fbf61674a7c0efb4a8c6575e8ddbc50ab0c
Rachit Gupta <rachitgupta75@gmail.com>
2017-01-13 01:15:35 -0800

    Made updates with the feedback from Priyanka Kodikal

--- js/bot.js
+++ js/bot.js
@@ -7,9 +7,10 @@ $(function() {
 			loop:false
 		})
 		.fbMessenger('start', { delay: 0 })
-		.fbMessenger('typingIndicator', { delay: 500 })
-		.fbMessenger('message', 'bot', 'Hi, I\'m Rachit - a product focused designer inspired by human emotions and curiosity.', { timestamp: now, delay: 500 })
-		.fbMessenger('message', 'bot', 'Currently, I\'m working at <b>Walmart Labs</b>.', { timestamp: now, delay: 500 })
+		.fbMessenger('typingIndicator', { delay: 1200 })
+		.fbMessenger('message', 'bot', 'Hi, I\'m Rachit - a product focused designer inspired by human emotions and curiousity.', { timestamp: now, delay: 800 })
+		.fbMessenger('message', 'bot', 'Currently, I\'m working at <b>Walmart Labs</b>.', { timestamp: now, delay: 1200 })
+		.fbMessenger('typingIndicator', { delay: 800 })
 
 		.fbMessenger('showGenericTemplate', [
 		{
@@ -18,18 +19,18 @@ $(function() {
 		subtitle: '',
 		buttons: []
 		}
-		], { delay: 200 })
+		], { delay: 1500 })
 
-		.fbMessenger('message', 'user', 'I see that you\'re into chats and messaging.', { timestamp: now, delay: 2000 })
-		.fbMessenger('typingIndicator', { delay: 500 })
-		.fbMessenger('message', 'bot', 'I sure am! Scroll down for my work or follow the links:', { timestamp: now, delay: 2000 })
+		.fbMessenger('message', 'user', 'I see that you\'re into chats and messaging experiences.', { timestamp: now, delay: 3000 })
+		.fbMessenger('typingIndicator', { delay: 2500 })
+		.fbMessenger('message', 'bot', 'Oh I sure am! Scroll down for my work or follow these:', { timestamp: now, delay: 1500 })
 		.fbMessenger('message', '', '', { timestamp: now, delay: 0 })
 		.fbMessenger('showQuickReplies', [
 			'<a href="#scroll-to-work" id="work-link">Work</a>',
 			'<a href="https://medium.com/@rachitgupta" target="_blank">Thoughts</a>',
 			'<a href="about.html" target="_blank">About</a>',
 			'<a href="resume.pdf" target="_blank">Resume</a>'
-		], { timestamp: now, delay: 500 })
+		], { timestamp: now, delay: 1500 })
 		// .fbMessenger('scrollQuickReplies', 3, { delay: 2000 })
 		// .fbMessenger('scrollQuickReplies', 0, { delay: 1000 })
 		// .fbMessenger('scrollQuickReplies', 4, { delay: 1000 })
