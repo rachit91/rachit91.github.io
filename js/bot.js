36b46348c87a2cceaff5f86378959004e87e00ad
Rachit Gupta <rachitgupta75@gmail.com>
2017-08-26 18:23:35 -0700

    changed bot convo, walmart page and added timestamp project

--- js/bot.js
+++ js/bot.js
@@ -6,43 +6,47 @@ $(function() {
 			botLogoUrl: 'img/logo-circ.svg',
 			loop:false
 		})
-		.fbMessenger('start', { delay: 0 })
-		.fbMessenger('typingIndicator', { delay: 1200 })
-		.fbMessenger('message', 'bot', 'Hi, I\'m Rachit - a product focused designer inspired by human emotions.', { timestamp: now, delay: 800 })
-		.fbMessenger('message', 'bot', 'Rolling beans at <b>Walmart Labs</b> 😆', { timestamp: now, delay: 1200 })
-		.fbMessenger('typingIndicator', { delay: 800 })
+	.fbMessenger('start', { delay: 0 })
+	.fbMessenger('typingIndicator', { delay: 1200 })
+	.fbMessenger('message', 'bot', 'When I was 6, my sister was half my age. Now, I\'m 26. How old is she?', { timestamp: now, delay: 800 })
+	.fbMessenger('message', 'user', '🤔 🤔', { timestamp: now, delay: 4500 })
+	.fbMessenger('typingIndicator', { delay: 2000 })
+	.fbMessenger('message', 'bot', 'Lol kidding, I\'m Rachit - UX Designer at Walmart Labs', { timestamp: now, delay: 1000 })
+	//.fbMessenger('message', 'bot', 'Rolling beans at <b>Walmart Labs</b> 😆', { timestamp: now, delay: 1200 })
 
-		.fbMessenger('showGenericTemplate', [
-		{
-		imageUrl: 'img/hey.gif',
-		title: '',
-		subtitle: '',
-		buttons: []
-		}
-		], { delay: 1500 })
+	.fbMessenger('showGenericTemplate', [
+	{
+	imageUrl: 'img/hey.gif',
+	title: '',
+	subtitle: '',
+	buttons: []
+	}
+	], { delay: 1500 })
 
-		.fbMessenger('message', 'user', 'I\'m here to see your work bruhh, let\'s see some action!', { timestamp: now, delay: 3000 })
-		.fbMessenger('typingIndicator', { delay: 2500 })
-		.fbMessenger('message', 'bot', 'Aiighht cools. <a href="https://rachit91.github.io/bundles-proto" target="_blank">Product Page - Bundles</a> is the latest. Not live yet but.. there\'s more', { timestamp: now, delay: 1500 })
-		.fbMessenger('message', '', '', { timestamp: now, delay: 0 })
-		.fbMessenger('showQuickReplies', [
-			'<a href="#scroll-to-work" id="work-link">Work</a>',
-			'<a href="https://medium.com/@rachitgupta" target="_blank">Writing</a>',
-			'<a href="about.html" target="_blank">About</a>',
-			'<a href="resume.pdf" target="_blank">Resume</a>'
-		], { timestamp: now, delay: 1500 })
-		// .fbMessenger('scrollQuickReplies', 3, { delay: 2000 })
-		// .fbMessenger('scrollQuickReplies', 0, { delay: 1000 })
-		// .fbMessenger('scrollQuickReplies', 4, { delay: 1000 })
-		// .fbMessenger('selectQuickReply', 2, { delay: 1200 })
-		// .fbMessenger('typingIndicator', { delay: 2000 })
-		// .fbMessenger('message', 'bot', 'Obvious choice, haha', { timestamp: now, delay: 1000 })
-		// .fbMessenger('typingIndicator', { delay: 200 })
-		// .fbMessenger('message', 'bot', 'You gotta scroll down now.. DO IT!', { timestamp: now, delay: 800 })
-		// .fbMessenger('message', '', '', { timestamp: now, delay: 0 })
-		// .fbMessenger('message', '', '', { timestamp: now, delay: 0 })
+	.fbMessenger('message', 'user', 'So, how old is she?', { timestamp: now, delay: 3000 })
+	.fbMessenger('typingIndicator', { delay: 2500 })
+	.fbMessenger('message', 'bot', 'haha, shoot me a message with your guess & I\'ll tell ya', { timestamp: now, delay: 1500 })
+	.fbMessenger('typingIndicator', { delay: 1500 })
+	.fbMessenger('message', 'bot', 'Meanwhile, below is my work, blog and more..nice to meet ya, user!', { timestamp: now, delay: 1500 })
+	.fbMessenger('message', '', '', { timestamp: now, delay: 0 })
+	.fbMessenger('showQuickReplies', [
+		'<a href="#scroll-to-work" id="work-link">Work</a>',
+		'<a href="https://medium.com/@rachitgupta" target="_blank">Writing</a>',
+		'<a href="about.html" target="_blank">About</a>',
+		'<a href="resume.pdf" target="_blank">Resume</a>'
+	], { timestamp: now, delay: 1500 })
+	// .fbMessenger('scrollQuickReplies', 3, { delay: 2000 })
+	// .fbMessenger('scrollQuickReplies', 0, { delay: 1000 })
+	// .fbMessenger('scrollQuickReplies', 4, { delay: 1000 })
+	// .fbMessenger('selectQuickReply', 2, { delay: 1200 })
+	// .fbMessenger('typingIndicator', { delay: 2000 })
+	// .fbMessenger('message', 'bot', 'Obvious choice, haha', { timestamp: now, delay: 1000 })
+	// .fbMessenger('typingIndicator', { delay: 200 })
+	// .fbMessenger('message', 'bot', 'You gotta scroll down now.. DO IT!', { timestamp: now, delay: 800 })
+	// .fbMessenger('message', '', '', { timestamp: now, delay: 0 })
+	// .fbMessenger('message', '', '', { timestamp: now, delay: 0 })
 
-		.fbMessenger('run');
+	.fbMessenger('run');
 });
 
 
